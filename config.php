87177a5fa598733a83d18c0e809fad3e3cda99dc
dahmanianais <anaisdahmn@gmail.com>
2026-04-23 23:46:52 +0200

    Change PDO connection to include port

--- config.php
+++ config.php
@@ -14,7 +14,7 @@ $user = 'root';
 $pass = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
